488951aa21125856f8f5301e5b97d95dc2e35aa5
nubiacantu <72423817+nubiacantu@users.noreply.github.com>
2023-07-05 07:22:41 -0600

    Update app.js

--- venta/resources/js/app.js
+++ venta/resources/js/app.js
@@ -1,3 +1,8 @@
 import './bootstrap';
 
-import 'select2';
+import './plugins/chartjs.min.js';
+import './plugins/Chart.extension.js';
+import './plugins/perfect-scrollbar.min.js';
+import './argon-dashboard-tailwind.js';
+import './argon-dashboard-tailwind.min.js';
+import './navbar-sticky.js';
