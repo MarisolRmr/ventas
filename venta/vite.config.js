5ccc2d29a856c43df1b94c3cd7a4fb87938bb1dd
root <2030210>
2023-07-09 19:45:30 +0000

    estilos

--- venta/vite.config.js
+++ venta/vite.config.js
@@ -5,20 +5,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
-         input: [
-         'resources/css/app.css',
-         'resources/js/app.js',
-         'resources/css/nucleo-icons.css',
-         'resources/css/nucleo-svg.css',
-         'resources/css/argon-dashboard-tailwind.css',
-         'resources/js/plugins/chartjs.min.js',
-         'resources/js/plugins/Chart.extension.js',
-         'resources/js/plugins/perfect-scrollbar.min.js',
-         'resources/js/argon-dashboard-tailwind.js',
-         'resources/js/argon-dashboard-tailwind.min.js',
-         'resources/js/navbar-sticky.js',
-=======
             input: ['resources/css/app.css', 
 'resources/css/nucleo-icons.css',
 'resources/css/nucleo-svg.css',
@@ -30,10 +16,7 @@ export default defineConfig({
 'resources/js/argon-dashboard-tailwind.js',
 'resources/js/argon-dashboard-tailwind.min.js',
 'resources/js/navbar-sticky.js'],
-            refresh: true,
-        }),
->>>>>>> 77bcb33 (cambios server)
-    ],
+       
      refresh: true,
  }),
  ],
