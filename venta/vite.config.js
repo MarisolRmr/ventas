b4c24396bcf621f8cffe1a2b285b131274a17d1b
nubiacantu <72423817+nubiacantu@users.noreply.github.com>
2023-07-09 13:56:34 -0600

    Update vite.config.js

--- venta/vite.config.js
+++ venta/vite.config.js
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vite';
-
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 
-'resources/css/nucleo-icons.css',
-'resources/css/nucleo-svg.css',
-'resources/css/argon-dashboard-tailwind.css',
-'resources/js/app.js',
-'resources/js/plugins/chartjs.min.js',
-'resources/js/plugins/Chart.extension.js',
-'resources/js/plugins/perfect-scrollbar.min.js',
-'resources/js/argon-dashboard-tailwind.js',
-'resources/js/argon-dashboard-tailwind.min.js',
-'resources/js/navbar-sticky.js'],
-       
-     refresh: true,
- }),
- ],
+            'resources/js/app.js',
+            'resources/vendor/nucleo/css/nucleo.css',
+            'resources/vendor/@fortawesome/fontawesome-free/css/all.min.css',
+            'resources/css/argon.css',
+           'resources/vendor/jquery/dist/jquery.min.js',
+           'resources/vendor/bootstrap/dist/js/bootstrap.bundle.min.js',
+           'resources/vendor/js-cookie/js.cookie.js',
+           'resources/vendor/jquery.scrollbar/jquery.scrollbar.min.js',
+           'resources/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js',
+           'resources/vendor/chart.js/dist/Chart.min.js',
+           'resources/vendor/chart.js/dist/Chart.extension.js',
+           'resources/js/argon.js',
+        ],
+            refresh: true,
+        }),
+    ],
 });
