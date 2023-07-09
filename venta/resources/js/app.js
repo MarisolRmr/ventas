633d3993f4dffedd32fdd277cae8302db226fc34
nubiacantu <72423817+nubiacantu@users.noreply.github.com>
2023-07-09 13:55:21 -0600

    Update app.js

--- venta/resources/js/app.js
+++ venta/resources/js/app.js
@@ -1,12 +1,2 @@
 import './bootstrap';
 
-import './plugins/chartjs.min.js';
-import './plugins/Chart.extension.js';
-import './plugins/perfect-scrollbar.min.js';
-import './argon-dashboard-tailwind.js';
-import './argon-dashboard-tailwind.min.js';
-<<<<<<< HEAD
-=======
-
->>>>>>> 77bcb33 (cambios server)
-import './navbar-sticky.js';
